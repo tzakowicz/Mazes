ec76cab4ca825ce98d9234b6de3e1bb02aa9485a
gilmorne <gilmorne@5da97f6d-6b7b-c04a-812e-24411449db11>
2019-05-19 15:30:18 +0000

    switched to JBoss for service integration. Added deployment descriptor.

--- Mazes/trunk/maze-service/src/main/java/net/gilmor/service/maze/MazeService.java
+++ Mazes/trunk/maze-service/src/main/java/net/gilmor/service/maze/MazeService.java
@@ -7,9 +7,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -20,42 +18,49 @@ import net.gilmor.service.maze.model.MazeMap;
 
 @Path("/maze")
 public class MazeService {
-
-	@GET
-	@Path("/image")
-	public Response getMazeImage(
-			@DefaultValue("10") @QueryParam("ratio") int ratio,
-			@DefaultValue("20") @QueryParam("height") int height,
-			@DefaultValue("20") @QueryParam("width") int width) {
-		Maze maze = new Maze(height,width);
-		maze.initGrid();
-		BufferedImage img = new MazeImage(maze)
-				.setRatio(ratio)
-				.buildImage()
-				.getImage();
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try {
-			ImageIO.write(img, "bmp", baos);
-		} catch (IOException e) {
-			return Response.status(Status.INTERNAL_SERVER_ERROR)
-					.header("Content-Type", "text/plain")
-					.entity(e.getMessage())
-					.build();
-		}
-		return Response.ok(baos.toByteArray())
-				.header("Content-Type", "image/bmp")
-				.build();
-				
-	}
 	
 	@GET
-	@Path("/map")
-	public MazeMap getMazeMap(
-			@DefaultValue("20") @QueryParam("width") int width,
-			@DefaultValue("20") @QueryParam("height") int height) {
-		Maze maze = new Maze(width, height);
-		maze.initGrid();
-		MazeMap map = MazeMap.consume(maze);
-		return map;
+	public Response getResponse() {
+		return Response
+				.ok("Succeed")
+				.build();
 	}
+
+//	@GET
+//	@Path("/image")
+//	public Response getMazeImage(
+//			@DefaultValue("10") @QueryParam("ratio") int ratio,
+//			@DefaultValue("20") @QueryParam("height") int height,
+//			@DefaultValue("20") @QueryParam("width") int width) {
+//		Maze maze = new Maze(height,width);
+//		maze.initGrid();
+//		BufferedImage img = new MazeImage(maze)
+//				.setRatio(ratio)
+//				.buildImage()
+//				.getImage();
+//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//		try {
+//			ImageIO.write(img, "bmp", baos);
+//		} catch (IOException e) {
+//			return Response.status(Status.INTERNAL_SERVER_ERROR)
+//					.header("Content-Type", "text/plain")
+//					.entity(e.getMessage())
+//					.build();
+//		}
+//		return Response.ok(baos.toByteArray())
+//				.header("Content-Type", "image/bmp")
+//				.build();
+//				
+//	}
+//	
+//	@GET
+//	@Path("/map")
+//	public MazeMap getMazeMap(
+//			@DefaultValue("20") @QueryParam("width") int width,
+//			@DefaultValue("20") @QueryParam("height") int height) {
+//		Maze maze = new Maze(width, height);
+//		maze.initGrid();
+//		MazeMap map = MazeMap.consume(maze);
+//		return map;
+//	}
 }
--- Mazes/trunk/maze-ui-javafx/src/main/java/net/gilmor/mazeui/main/Main.java
+++ Mazes/trunk/maze-ui-javafx/src/main/java/net/gilmor/mazeui/main/Main.java
@@ -1,13 +1,11 @@
 package net.gilmor.mazeui.main;
 
-import javafx.application.Application;
 import net.gilmor.mazeui.application.MazeApplication;
 
 @SuppressWarnings("restriction")
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		Application app = new MazeApplication();
-		app.launch(args);
+		MazeApplication.launch(args);
 	}
 }
