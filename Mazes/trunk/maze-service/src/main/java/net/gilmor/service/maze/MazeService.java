3f42389bbc9c4c1cae61ffec98022d87c4ce5b96
gilmorne <gilmorne@5da97f6d-6b7b-c04a-812e-24411449db11>
2019-05-22 22:34:37 +0000

    got service working.

--- Mazes/trunk/maze-service/src/main/java/net/gilmor/service/maze/MazeService.java
+++ Mazes/trunk/maze-service/src/main/java/net/gilmor/service/maze/MazeService.java
@@ -9,58 +9,55 @@ import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import com.google.gson.Gson;
+
+import maze.algorithms.RandomLeafWalkMaze;
 import maze.display.MazeImage;
 import maze.parent.Maze;
 import net.gilmor.service.maze.model.MazeMap;
 
-@Path("/maze")
-public class MazeService {
+@Path("/")
+public class MazeService extends Application {
 	
 	@GET
-	public Response getResponse() {
-		return Response
-				.ok("Succeed")
-				.build();
+	@Path("/map")
+	public Response getMazeMap(
+			@DefaultValue("20") @QueryParam("width") int width,
+			@DefaultValue("20") @QueryParam("height") int height) {
+		Maze maze = new RandomLeafWalkMaze(width, height);
+		maze.initGrid();
+		MazeMap map = MazeMap.consume(maze);
+		Gson gson = new Gson();
+		String jsonMap = gson.toJson(map);
+		return Response.ok(jsonMap).build();
 	}
 
-//	@GET
-//	@Path("/image")
-//	public Response getMazeImage(
-//			@DefaultValue("10") @QueryParam("ratio") int ratio,
-//			@DefaultValue("20") @QueryParam("height") int height,
-//			@DefaultValue("20") @QueryParam("width") int width) {
-//		Maze maze = new Maze(height,width);
-//		maze.initGrid();
-//		BufferedImage img = new MazeImage(maze)
-//				.setRatio(ratio)
-//				.buildImage()
-//				.getImage();
-//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//		try {
-//			ImageIO.write(img, "bmp", baos);
-//		} catch (IOException e) {
-//			return Response.status(Status.INTERNAL_SERVER_ERROR)
-//					.header("Content-Type", "text/plain")
-//					.entity(e.getMessage())
-//					.build();
-//		}
-//		return Response.ok(baos.toByteArray())
-//				.header("Content-Type", "image/bmp")
-//				.build();
-//				
-//	}
-//	
-//	@GET
-//	@Path("/map")
-//	public MazeMap getMazeMap(
-//			@DefaultValue("20") @QueryParam("width") int width,
-//			@DefaultValue("20") @QueryParam("height") int height) {
-//		Maze maze = new Maze(width, height);
-//		maze.initGrid();
-//		MazeMap map = MazeMap.consume(maze);
-//		return map;
-//	}
+	@GET
+	@Path("/image")
+	public Response getMazeImage(
+			@DefaultValue("10") @QueryParam("ratio") int ratio,
+			@DefaultValue("20") @QueryParam("height") int height,
+			@DefaultValue("20") @QueryParam("width") int width) {
+		Maze maze = new RandomLeafWalkMaze(height,width);
+		maze.initGrid();
+		BufferedImage img = new MazeImage(maze)
+				.setRatio(ratio)
+				.buildImage()
+				.getImage();
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			ImageIO.write(img, "bmp", baos);
+			return Response.ok(baos.toByteArray())
+					.header("Content-Type", "image/bmp")
+					.build();
+		} catch (IOException e) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR)
+					.header("Content-Type", "text/plain")
+					.entity(e.getMessage())
+					.build();
+		}
+	}
 }
