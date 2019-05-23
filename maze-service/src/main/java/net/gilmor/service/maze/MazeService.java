7b13e890e56d3eb47c5297bc0bd68247c6ac8294
gilmorne <86752466+tzakowicz@users.noreply.github.com>
2019-05-23 23:35:09 +0000

    Extraneous import.

--- maze-service/src/main/java/net/gilmor/service/maze/MazeService.java
+++ maze-service/src/main/java/net/gilmor/service/maze/MazeService.java
@@ -9,7 +9,6 @@ import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
