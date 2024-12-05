fe0c734d528ac297b71d89f8ce6598fb48f030a3
Thomas Zakowicz <tzakowicz@gmail.com>
2024-12-05 11:49:16 -0500

    get the application running on tomee 9.1.3

--- maze-parent/maze-service/src/main/java/net/gilmor/service/maze/beans/MazeBean.java
+++ maze-parent/maze-service/src/main/java/net/gilmor/service/maze/beans/MazeBean.java
@@ -2,9 +2,8 @@ package net.gilmor.service.maze.beans;
 
 import java.io.Serializable;
 
-import javax.ejb.Stateful;
-import javax.enterprise.context.SessionScoped;
-
+import jakarta.ejb.Stateful;
+import jakarta.enterprise.context.SessionScoped;
 import maze.algorithms.RandomLeafWalkMaze;
 import maze.parent.PlayableMaze;
 
@@ -12,60 +11,60 @@ import maze.parent.PlayableMaze;
 @SessionScoped
 public class MazeBean implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
-	private PlayableMaze maze;
-	private long startTime;
-	
-	public void MazeGame() {
-		startGame(20,20);
-	}
-	
-	public PlayableMaze startGame(int height, int width) {
-		maze = new RandomLeafWalkMaze(height, width);
-		maze.initGrid();
-		startTime = System.currentTimeMillis();
-		return maze;
-	}
-	
-	public int getPlayerX() {
-		return maze.getPlayerRow();
-	}
-	
-	public int getPlayerY() {
-		return maze.getPlayerCol();
-	}
-	
-	public int getFinishX() {
-		return maze.getFinishRow();
-	}
-	
-	public int getFinishY() {
-		return maze.getFinishCol();
-	}
-	
-	public long getPlayTime() {
-		long currentTime = System.currentTimeMillis();
-		return currentTime - startTime;
-	}
-	
-	public boolean isFinished() {
-		return maze.checkWin();
-	}
-	
-	public void moveUp() {
-		maze.moveNorth();
-	}
-	
-	public void moveDown() {
-		maze.moveSouth();
-	}
-	
-	public void moveLeft() {
-		maze.moveWest();
-	}
-	
-	public void moveRight() {
-		maze.moveEast();
-	}
+    private static final long serialVersionUID = 1L;
+
+    private PlayableMaze maze;
+    private long startTime;
+
+    public void MazeGame() {
+        startGame(20, 20);
+    }
+
+    public PlayableMaze startGame(int height, int width) {
+        maze = new RandomLeafWalkMaze(height, width);
+        maze.initGrid();
+        startTime = System.currentTimeMillis();
+        return maze;
+    }
+
+    public int getPlayerX() {
+        return maze.getPlayerRow();
+    }
+
+    public int getPlayerY() {
+        return maze.getPlayerCol();
+    }
+
+    public int getFinishX() {
+        return maze.getFinishRow();
+    }
+
+    public int getFinishY() {
+        return maze.getFinishCol();
+    }
+
+    public long getPlayTime() {
+        long currentTime = System.currentTimeMillis();
+        return currentTime - startTime;
+    }
+
+    public boolean isFinished() {
+        return maze.checkWin();
+    }
+
+    public void moveUp() {
+        maze.moveNorth();
+    }
+
+    public void moveDown() {
+        maze.moveSouth();
+    }
+
+    public void moveLeft() {
+        maze.moveWest();
+    }
+
+    public void moveRight() {
+        maze.moveEast();
+    }
 }
